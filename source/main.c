5ac3ea86761c74e3b0a9f7433a09f552df670909
Krasaar <a.ban1245@gmail.com>
2024-10-23 08:54:08 +0200

    improved CMakeLists.txt, ignored .vscode and poked main.c

--- source/main.c
+++ source/main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+typedef struct addrinfo addrinfo; 
 
 int main()
 {
-	printf("Hello World!\n");
+	addrinfo hints;
+	addrinfo* server_info;
+	int status;
+
+	memset(&hints, 0, sizeof hints); // make sure the struct is empty
+	hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+
+	status = getaddrinfo("www.example.net", "3490", &hints, &server_info);
+
+	freeaddrinfo(server_info);
 	return 0;
 }
