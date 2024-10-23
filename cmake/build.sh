789cbc00c64601bc47635d0fef68d4961177c79e
Krasaar <a.ban1245@gmail.com>
2024-10-23 09:25:20 +0200

    some more bash / cmakelists changes! Yay!

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+build_type="$1"
+if [ -z "$build_type" ]; then
+	build_type="Debug"
+fi
+
 if [ ! -d cmake/generated ]; then
   mkdir cmake/cmake_auto_generated
 fi
-cmake -B cmake/cmake_auto_generated -S cmake -DCMAKE_BUILD_TYPE=Debug
+cmake -B cmake/cmake_auto_generated -S cmake -DCMAKE_BUILD_TYPE=$build_type
 cmake --build cmake/cmake_auto_generated
